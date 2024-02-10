1372cd17a226f28f4985d44da91d8f408b5544d4
FIFSAK <anuar200572@gmail.com>
2024-02-10 12:53:22 +0600

    works

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import time
-import selenium
+from selenium.webdriver.support import expected_conditions as EC
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from dotenv import load_dotenv
 import os
 
+from selenium.webdriver.support.wait import WebDriverWait
+
 load_dotenv()
-
-login = os.getenv("login")
-password = os.getenv("password")
-
 
 login = os.getenv("login")
 password = os.getenv("password")
@@ -22,24 +21,39 @@ driver = webdriver.Chrome()
 driver.get("https://wsp.kbtu.kz/RegistrationOnline")
 while True:
     try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="gwt-uid-4"]'))
+        )
+        login_form = driver.find_element(By.XPATH, '//*[@id="gwt-uid-4"]')
+        password_form = driver.find_element(By.XPATH, '//*[@id="gwt-uid-6"]')
+
+        login_form.send_keys(login)
+        time.sleep(2)
+        password_form.send_keys(password)
+        time.sleep(2)
+        password_form.send_keys(Keys.ENTER)
         time.sleep(5)
-        login_form = driver.find_elements(By.XPATH, '//*[@id="gwt-uid-4"]')
-        password_form = driver.find_elements(By.XPATH, '//*[@id="gwt-uid-6"]')
-        buttons = driver.find_elements(By.XPATH, "//span[@class='v-button-caption' and contains(text(), 'Отметиться')]")
-        if login_form and password:
-            login_form[0].send_keys(login)
-            password_form[0].send_keys(password)
-            password_form[0].send_keys(Keys.ENTER)
-            time.sleep(5)
-        else:
-            print("form not found")
+
+
+
+    except:
+        print("Элемент для авторизации не найден ищем элемент для отметки")
+        buttons = driver.find_elements(By.XPATH,'//*[@id="RegistrationOnline-1674962804"]/div/div[2]/div/div[2]/div/div/div/div/div/div/div[3]/div')
+        print(buttons)
         if buttons:
             for button in buttons:
-                button.click()
-        else:
-            print("Кнопка 'Отметиться' не найдена")
-            time.sleep(5)
-            driver.refresh()
-    except NoSuchElementException:
-        print("Элемент не найден")
+                try:
+                    button.click()
+                    print("click")
+                except StaleElementReferenceException:
+                    print("Элемент устарел, ищем заново")
+                    print("обнавляю")
+                    time.sleep(60)
+                    driver.refresh()
+                    # Повторно ищем элементы и кликаем, если это необходимо
+                    # Возможно, потребуется обновить XPath или логику поиска
+        print("обнавляю")
+        time.sleep(60)
+        driver.refresh()
+
 
