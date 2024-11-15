c9d70a0a18c2e76c1e2583153ecc35f2cf9eb0e6
FIFSAK <anuar200572@gmail.com>
2024-11-15 17:12:52 +0500

    change if statements

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ try:
             time.sleep(2)
             password_form.send_keys(Keys.ENTER)
             time.sleep(5)
-            print("I am logged in")
+            # print("I am logged in")
 
 
 
@@ -46,13 +46,11 @@ try:
             # print("auth form not found will find attend element")
             buttons = driver.find_elements(By.XPATH,
                                            '//*[@id="RegistrationOnline-1674962804"]/div/div[2]/div/div[2]/div/div/div/div/div/div/div[3]/div')
-            # print(buttons)
 
             if buttons:
                 for button in buttons:
                     class_attribute = button.get_attribute("class")
-                    # if True:
-                    if "v-enabled" in class_attribute:
+                    if "v-disabled" not in class_attribute:
                         try:
                             button.click()
                             # print("click attend")
@@ -63,12 +61,12 @@ try:
                         except:
                             bot.send_message(chat_id, "I can't click attend, click by yourself")
                             continue
-                    print("no attend button")
-            print("refreshing")
+                    # print("no attend button")
+            # print("refreshing")
             # bot.send_message(chat_id, "I will refresh the page")
             time.sleep(60)
             driver.refresh()
 except Exception as e:
     bot.send_message(chat_id, "I am stopping the bot")
     bot.send_message(chat_id, e)
-    print(e)
+    # print(e)
